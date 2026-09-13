248d4d1bb1bdc4e6f0ddf7680f81ee55c2cb1e98
Pandanobita1993 <minhtien.040393@gmail.com>
2026-09-13 14:35:23 +0700

    Update App_Hoc_Tap.py

--- App_Hoc_Tap.py
+++ App_Hoc_Tap.py
@@ -113,7 +113,7 @@ def set_kids_background(image_path):
         </style>
     """, unsafe_allow_html=True)
 
-set_kids_background("Background.png")
+set_kids_background("Background.jpg")
 
 st.markdown("<h1 style='text-align: center;'>🌟 CAMBRIDGE KIDS LMS 🌟</h1>", unsafe_allow_html=True)
 
