f111dad47374c1739c304f94adc51310d76cd525
Pandanobita1993 <minhtien.040393@gmail.com>
2026-09-13 18:57:14 +0700

    Update App_Hoc_Tap.py

--- App_Hoc_Tap.py
+++ App_Hoc_Tap.py
@@ -64,7 +64,7 @@ def set_responsive_background(image_path, current_role):
         bg_css = 'background-color: #f5f6fa;'
     
     # Độ trong suốt: 
-    bg_opacity = "rgba(255, 255, 255, 0.15)" if current_role is None else "rgba(255, 255, 255, 0.55)"
+    bg_opacity = "rgba(255, 255, 255, 0.05)" if current_role is None else "rgba(255, 255, 255, 0.95)"
 
     st.markdown(f"""
         <style>
